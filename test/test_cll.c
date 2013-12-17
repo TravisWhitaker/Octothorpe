8ab537d9a18d3cf25309a99e14729697d2530b70
Travis Whitaker <twhitak@its.jnj.com>
2013-12-17 14:32:44 +0000

    Implemented fast fetches for cll's with tests.

--- test/test_cll.c
+++ test/test_cll.c
@@ -68,10 +68,10 @@ int main()
 		printf("test_cll: FAILED: octo_cll_poke found non-existent key\n");
 		return 1;
 	}
-	printf("test_cll: Fetching inserted records...\n");
-	void *output1 = octo_cll_fetch(key1, (const octo_dict_cll_t *)test_cll);
-	void *output2 = octo_cll_fetch(key2, (const octo_dict_cll_t *)test_cll);
-	void *output3 = octo_cll_fetch(key3, (const octo_dict_cll_t *)test_cll);
+	printf("test_cll: Fetching inserted records \"safely\"...\n");
+	void *output1 = octo_cll_fetch_safe(key1, (const octo_dict_cll_t *)test_cll);
+	void *output2 = octo_cll_fetch_safe(key2, (const octo_dict_cll_t *)test_cll);
+	void *output3 = octo_cll_fetch_safe(key3, (const octo_dict_cll_t *)test_cll);
 	if(output1 == NULL || output2 == NULL || output3 == NULL)
 	{
 		printf("test_cll: FAILED: octo_cll_fetch returned NULL\n");
@@ -101,6 +101,36 @@ int main()
 	free(output1);
 	free(output2);
 	free(output3);
+	printf("test_cll: Fetching inserted records \"unsafely\"...\n");
+	output1 = octo_cll_fetch(key1, (const octo_dict_cll_t *)test_cll);
+	output2 = octo_cll_fetch(key2, (const octo_dict_cll_t *)test_cll);
+	output3 = octo_cll_fetch(key3, (const octo_dict_cll_t *)test_cll);
+	if(output1 == NULL || output2 == NULL || output3 == NULL)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned NULL\n");
+		return 1;
+	}
+	if(output1 == (void *)test_cll || output2 == (void *)test_cll || output3 == (void *)test_cll)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch couldn't find test value\n");
+		return 1;
+	}
+	printf("test_cll: Checking for correct values...\n");
+	if(memcmp(val1, output1, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"abcdefg\\0\"\n");
+		return 1;
+	}
+	if(memcmp(val2, output2, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"bcdefgh\\0\"\n");
+		return 1;
+	}
+	if(memcmp(val3, output3, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"cdefghi\\0\"\n");
+		return 1;
+	}
 	printf("test_cll: Looking up non-existent key...\n");
 	void *error_output = octo_cll_fetch("zxcvbde\0", (const octo_dict_cll_t *)test_cll);
 	if(error_output == NULL)
@@ -139,10 +169,10 @@ int main()
 		printf("test_cll: FAILED: octo_cll_poke found non-existent key\n");
 		return 1;
 	}
-	printf("test_cll: Fetching inserted records...\n");
-	output1 = octo_cll_fetch(key1, (const octo_dict_cll_t *)test_cll);
-	output2 = octo_cll_fetch(key2, (const octo_dict_cll_t *)test_cll);
-	output3 = octo_cll_fetch(key3, (const octo_dict_cll_t *)test_cll);
+	printf("test_cll: Fetching inserted records \"safely\"...\n");
+	output1 = octo_cll_fetch_safe(key1, (const octo_dict_cll_t *)test_cll);
+	output2 = octo_cll_fetch_safe(key2, (const octo_dict_cll_t *)test_cll);
+	output3 = octo_cll_fetch_safe(key3, (const octo_dict_cll_t *)test_cll);
 	if(output1 == NULL || output2 == NULL || output3 == NULL)
 	{
 		printf("test_cll: FAILED: octo_cll_fetch returned NULL\n");
@@ -172,6 +202,36 @@ int main()
 	free(output1);
 	free(output2);
 	free(output3);
+	printf("test_cll: Fetching inserted records \"unsafely\"...\n");
+	output1 = octo_cll_fetch(key1, (const octo_dict_cll_t *)test_cll);
+	output2 = octo_cll_fetch(key2, (const octo_dict_cll_t *)test_cll);
+	output3 = octo_cll_fetch(key3, (const octo_dict_cll_t *)test_cll);
+	if(output1 == NULL || output2 == NULL || output3 == NULL)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned NULL\n");
+		return 1;
+	}
+	if(output1 == (void *)test_cll || output2 == (void *)test_cll || output3 == (void *)test_cll)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch couldn't find test value\n");
+		return 1;
+	}
+	printf("test_cll: Checking for correct values...\n");
+	if(memcmp(val1, output1, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"abcdefg\\0\"\n");
+		return 1;
+	}
+	if(memcmp(val2, output2, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"bcdefgh\\0\"\n");
+		return 1;
+	}
+	if(memcmp(val3, output3, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"cdefghi\\0\"\n");
+		return 1;
+	}
 	printf("test_cll: Looking up non-existent key...\n");
 	error_output = octo_cll_fetch("zxcvbde\n", (const octo_dict_cll_t *)test_cll);
 	if(error_output == NULL)
@@ -216,10 +276,10 @@ int main()
 		printf("test_cll: FAILED: octo_cll_poke found non-existent key\n");
 		return 1;
 	}
-	printf("test_cll: Fetching inserted records...\n");
-	output1 = octo_cll_fetch(key1, (const octo_dict_cll_t *)test_cll_safe);
-	output2 = octo_cll_fetch(key2, (const octo_dict_cll_t *)test_cll_safe);
-	output3 = octo_cll_fetch(key3, (const octo_dict_cll_t *)test_cll_safe);
+	printf("test_cll: Fetching inserted records \"safely\"...\n");
+	output1 = octo_cll_fetch_safe(key1, (const octo_dict_cll_t *)test_cll_safe);
+	output2 = octo_cll_fetch_safe(key2, (const octo_dict_cll_t *)test_cll_safe);
+	output3 = octo_cll_fetch_safe(key3, (const octo_dict_cll_t *)test_cll_safe);
 	if(output1 == NULL || output2 == NULL || output3 == NULL)
 	{
 		printf("test_cll: FAILED: octo_cll_fetch returned NULL\n");
@@ -249,6 +309,36 @@ int main()
 	free(output1);
 	free(output2);
 	free(output3);
+	printf("test_cll: Fetching inserted records \"unsafely\"...\n");
+	output1 = octo_cll_fetch(key1, (const octo_dict_cll_t *)test_cll_safe);
+	output2 = octo_cll_fetch(key2, (const octo_dict_cll_t *)test_cll_safe);
+	output3 = octo_cll_fetch(key3, (const octo_dict_cll_t *)test_cll_safe);
+	if(output1 == NULL || output2 == NULL || output3 == NULL)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned NULL\n");
+		return 1;
+	}
+	if(output1 == (void *)test_cll_safe || output2 == (void *)test_cll_safe || output3 == (void *)test_cll_safe)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch couldn't find test value\n");
+		return 1;
+	}
+	printf("test_cll: Checking for correct values...\n");
+	if(memcmp(val1, output1, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"abcdefg\\0\"\n");
+		return 1;
+	}
+	if(memcmp(val2, output2, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"bcdefgh\\0\"\n");
+		return 1;
+	}
+	if(memcmp(val3, output3, 64) != 0)
+	{
+		printf("test_cll: FAILED: octo_cll_fetch returned pointer to incorrect value for key \"cdefghi\\0\"\n");
+		return 1;
+	}
 	printf("test_cll: Looking up non-existent key...\n");
 	error_output = octo_cll_fetch("zxcvbde\n", (const octo_dict_cll_t *)test_cll_safe);
 	if(error_output == NULL)
