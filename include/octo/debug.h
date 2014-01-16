3b865f9e98d9ddd41db7a398cd0cb4bc0a2146e2
Travis Whitaker <pi.boy.travis@gmail.com>
2014-01-16 14:26:40 -0500

    Improved delimiter choice in DEBUG_MSG() macro expansion.

--- include/octo/debug.h
+++ include/octo/debug.h
@@ -4,7 +4,7 @@
 #define OCTO_DEBUG_H
 
 #ifdef DEBUG_MSG_ENABLE
-#define DEBUG_MSG(msg) printf("libocto: %s:%d: %s: %s\n", __FILE__, __LINE__, __func__, msg)
+#define DEBUG_MSG(msg) printf("libocto: %s:%d |-> %s: %s\n", __FILE__, __LINE__, __func__, msg)
 #else
 #define DEBUG_MSG(ignore) ((void) 0)
 #endif
