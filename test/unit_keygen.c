c2c4c2976ab0011086d52b82e16de325732d5ce3
Travis Whitaker <pi.boy.travis@gmail.com>
2014-02-12 17:41:39 -0500

    Tests are quieter and take advantage of DEBUG_MSG()

--- test/unit_keygen.c
+++ test/unit_keygen.c
@@ -8,15 +8,18 @@
 
 #include <octo/types.h>
 #include <octo/keygen.h>
+#include <octo/debug.h>
 
 int main()
 {
-	printf("test_keygen: Generating key...\n");
+	DEBUG_MSG("test_keygen: Generating key...\n");
 	uint8_t *test_key = octo_keygen();
-	printf("test_keygen: Got key ");
+	DEBUG_MSG("test_keygen: Got key ");
+	char buf[4];
 	for(unsigned int i = 0; i < 16; i++)
 	{
-		printf("%.2x ", test_key[i]);
+		snprintf(buf, 4, "%.2x", test_key[i]);
+		DEBUG_MSG(buf);
 	}
 	free(test_key);
 	printf("\ntest_keygen: SUCCESS!\n");
